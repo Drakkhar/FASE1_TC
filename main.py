93849fa61b650aa866d833dc13ff6f6b769e0306
Erick Navevaiko Moreira <erick.navevaiko@gmail.com>
2025-05-31 18:16:57 -0300

    Criação das rotas GET para chamada das funções de Web Scraping

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Base, engine
 from scraper import (
-    salvar_producao,
-    salvar_processamento,
-    salvar_comercializacao,
-    salvar_importacao,
-    salvar_exportacao
+    producao_rt,
+    comercializacao_rt,
+    processamento_rt,
+    importacao_rt,
+    exportacao_rt
 )
 
 # Criação do banco e tabelas
@@ -20,13 +20,64 @@ app = FastAPI(title="Vitibrasil Scraper API")
 def home():
     return {"mensagem": "API do Projeto Tech Challenger - Vitibrasil"}
 
-@app.post("/scrap/producao")
-def run_producao():
-    salvar_producao()
-    return {"status": "Producao salva com sucesso"}
+# Rota GET para consulta dos dados de Produção
+@app.get("/producao/{ano}")
+def get_producao(ano: int):
+    try:
+        dados = producao_rt(ano)
+        return {"Status": "Consulta realizada com sucesso", "Ano": ano, "Dados Produção": dados}
+    except ValueError as ve:
+        raise HTTPException(status_code=404, Erro=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, Erro=f"Erro interno: {str(e)}")
+
+# Rota GET para consulta dos dados de Processamento    
+@app.get("/processamento/{ano}/{opcao}")
+def get_processamento(ano: int, opcao: int):
+    try:
+        dados = processamento_rt(ano, opcao)
+        return {"Status": "Consulta realizada com sucesso", "Ano": ano, "Tipo": opcao, "Dados Processamento": dados}
+    except ValueError as ve:
+        raise HTTPException(status_code=404, Erro=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, Erro=f"Erro interno: {str(e)}")        
+
+
+# Rota GET para consulta dos dados de Comercialização    
+@app.get("/comercializacao/{ano}")
+def get_comercializacao(ano: int):
+    try:
+        dados = comercializacao_rt(ano)
+        return {"Status": "Consulta realizada com sucesso", "Ano": ano, "Dados Comercialização": dados}
+    except ValueError as ve:
+        raise HTTPException(status_code=404, Erro=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, Erro=f"Erro interno: {str(e)}")
+    
+# Rota GET para consulta dos dados de Importação    
+@app.get("/importacao/{ano}/{opcao}")
+def get_importacao(ano: int, opcao: int):
+    try:
+        dados = importacao_rt(ano, opcao)
+        return {"Status": "Consulta realizada com sucesso", "Ano": ano, "Tipo": opcao, "Dados Processamento": dados}
+    except ValueError as ve:
+        raise HTTPException(status_code=404, Erro=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, Erro=f"Erro interno: {str(e)}")       
+
+# Rota GET para consulta dos dados de Processamento    
+@app.get("/exportacao/{ano}/{opcao}")
+def get_exportacao(ano: int, opcao: int):
+    try:
+        dados = exportacao_rt(ano, opcao)
+        return {"Status": "Consulta realizada com sucesso", "Ano": ano, "Tipo": opcao, "Dados Exportação": dados}
+    except ValueError as ve:
+        raise HTTPException(status_code=404, Erro=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, Erro=f"Erro interno: {str(e)}")           
 
 #@app.post("/scrap/processamento")
-#def run_processamento():
+#def run_processamento():c
 #    salvar_processamento()
 #    return {"status": "Processamento salvo com sucesso"}
 
